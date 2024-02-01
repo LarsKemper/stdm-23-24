7c9c70a90d694f680fdf1554842a33cb354551ce
Lukas Erker <118379290+Erker158@users.noreply.github.com>
2024-02-01 15:49:14 +0100

    Update fillDB.sql

---
+++ assets/sql/fillDB.sql
@@ -0,0 +1,5 @@
+INSERT INTO Mitglied (Nachname, Vorname, Geburtsdatum, Geschlecht, Trainerschein, ist_aktiv, Anschrift, hat_Haftpflicht)
+VALUES
+('Mustermann', 'Max', '1990-01-01', 'M', TRUE, TRUE, 'Musterstraße 1, Musterstadt', TRUE),
+('Musterfrau', 'Maria', '1995-03-15', 'W', FALSE, TRUE, 'Beispielweg 2, Beispielstadt', FALSE),
+('Grab', 'Manuel', '2003-05-04', 'M', FALSE, TRUE, 'Hundeweg 69, Dummmhausen', FALSE);
